46928c2321d7c7f3cc04abc52a40bbd58c5f8b73
Mohammad <moh.hossain98@gmail.com>
2022-10-18 15:59:15 -0400

    commit tree 0541f8e8d09a7a1432775e3f709b103261c43163

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,22 @@
 class Dev < ActiveRecord::Base
 
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+
+    def received_one?(item_name)
+        self.freebies.exists?(:item_name => item_name)
+    end
+
+
+    def give_away(dev, freebie)
+        
+        if(received_one?(freebie.item_name))
+            freebie.update(dev_id: dev.id)
+        else
+            "Can't"
+        end
+        #  freebie.update(dev_id: dev.id) if self.received_one?(freebie.item_name)
+    end
+
 end
